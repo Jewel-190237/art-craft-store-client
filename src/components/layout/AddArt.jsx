206617d63d63b7040b81869bd2a9ea6a2413e4cb
Jewel-190237 <190237@ku.ac.bd>
2024-04-26 15:10:27 +0600

    add art added

--- src/components/layout/AddArt.jsx
+++ src/components/layout/AddArt.jsx
@@ -1,41 +1,45 @@
-// import 'sweetalert2/src/sweetalert2.scss'
-// import Swal from 'sweetalert2/dist/sweetalert2.js'
-// import 'sweetalert2/src/sweetalert2.scss'
+import 'sweetalert2/src/sweetalert2.scss'
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+import 'sweetalert2/src/sweetalert2.scss'
 const AddArt = () => {
 
     const handleAddCoffee = event => {
         event.preventDefault()
         const form = event.target;
 
-        const name = form.name.value;
-        const chef = form.chef.value;
-        const supplier = form.supplier.value;
-        const taste = form.taste.value;
-        const category = form.category.value;
-        const details = form.details.value;
-        const photoUrl = form.photoUrl.value;
+        const product_name = form.product_name.value;
+        const sub_category_name = form.sub_category_name.value;
+        const price = form.price.value;
+        const rating = form.rating.value;
+        const customization = form.customization.value;
+        const processing_time = form.processing_time.value;
+        const stock_status = form.stock_status.value;
+        const description = form.description.value;
+        const user_name = form.user_name.value;
+        const user_email = form.user_email.value;
+        const photo_url = form.photo_url.value;
 
-        const newCoffee = { name, chef, supplier, taste, category, details, photoUrl }
-        console.log(newCoffee)
+        const newArt = { product_name, sub_category_name, price, rating, customization, processing_time, stock_status, description, user_name, user_email, photo_url }
+        console.log(newArt)
 
-        // fetch('http://localhost:4000/coffee', {
-        //     method: 'POST',
-        //     headers: {
-        //         'content-type': 'application/json'
-        //     },
-        //     body: JSON.stringify(newCoffee)
-        // })
-        //     .then(res => res.json())
-        //     .then(data => {
-        //         console.log(data)
-        //         Swal.fire({
-        //             icon: "success",
-        //             title: "Your work has been saved",
-        //             showConfirmButton: false,
-        //             timer: 2000
-        //           });
+        fetch('http://localhost:5000/arts', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newArt)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                Swal.fire({
+                    icon: "success",
+                    title: "Your work has been saved",
+                    showConfirmButton: false,
+                    timer: 2000
+                  });
 
-        //     })
+            })
     }
     return (
         <div>
@@ -45,37 +49,37 @@ const AddArt = () => {
                     <div className="md:flex gap-4 mb-4 ">
                         <div className="form-control md:w-1/2">
                             <span className="label-text">Name</span>
-                            <input type="text" name="name" placeholder="Enter Product Name" className="input input-bordered w-full" />
+                            <input type="text" name="product_name" placeholder="Enter Product Name" className="input input-bordered w-full" />
                         </div>
                         <div className="form-control md:w-1/2">
                             <span className="label-text">Sub Category Name</span>
-                            <input type="text" name="chef" placeholder="Enter Sub Category Name" className="input input-bordered w-full" />
+                            <input type="text" name="sub_category_name" placeholder="Enter Sub Category Name" className="input input-bordered w-full" />
                         </div>
                     </div>
                     <div className="md:flex gap-4 mb-4">
                         <div className="form-control md:w-1/2">
                             <span className="label-text">Price</span>
-                            <input type="text" name="supplier" placeholder="Enter product Price" className="input input-bordered w-full" />
+                            <input type="number" name="price" placeholder="Enter product Price" className="input input-bordered w-full" />
                         </div>
                         <div className="form-control md:w-1/2">
                             <span className="label-text">Rating</span>
-                            <input type="text" name="taste" placeholder="Enter product rating" className="input input-bordered w-full" />
+                            <input type="number" name="rating" placeholder="Enter product rating" className="input input-bordered w-full" />
                         </div>
                     </div>
                     <div className="md:flex gap-4 mb-4">
                         <div className="form-control md:w-1/2">
                             <span className="label-text">customization</span>
-                            <input type="text" name="category" placeholder="Enter customization Yes/No" className="input input-bordered w-full" />
+                            <input type="text" name="customization" placeholder="Enter customization Yes/No" className="input input-bordered w-full" />
                         </div>
                         <div className="form-control md:w-1/2">
                             <span className="label-text">Processing time</span>
-                            <input type="text" name="details" placeholder="Enter processing Time" className="input input-bordered w-full" />
+                            <input type="text" name="processing_time" placeholder="Enter processing Time" className="input input-bordered w-full" />
                         </div>
                     </div>
                     <div className="md:flex gap-4 mb-4">
                         <div className="form-control md:w-1/2">
                             <span className="label-text">Stock Status</span>
-                            <input type="text" name="category" placeholder="Enter Stock Status" className="input input-bordered w-full" />
+                            <input type="text" name="stock_status" placeholder="Enter Stock Status" className="input input-bordered w-full" />
                         </div>
                         <div className="form-control md:w-1/2">
                             <span className="label-text">Description</span>
@@ -85,17 +89,17 @@ const AddArt = () => {
                     <div className="md:flex gap-4 mb-4">
                         <div className="form-control md:w-1/2">
                             <span className="label-text">User Name</span>
-                            <input type="text" name="name" placeholder="Enter user name" className="input input-bordered w-full" />
+                            <input type="text" name="user_name" placeholder="Enter user name" className="input input-bordered w-full" />
                         </div>
                         <div className="form-control md:w-1/2">
                             <span className="label-text">Email</span>
-                            <input type="email" name="email" placeholder="Enter email address" className="input input-bordered w-full" />
+                            <input type="email" name="user_email" placeholder="Enter email address" className="input input-bordered w-full" />
                         </div>
                     </div>
                     <div className="md:flex gap-4 mb-4 ">
                         <div className="form-control w-full">
                             <span className="label-text">Photo url</span>
-                            <input type="text" name="photo" placeholder="Enter Photo url" className="input input-bordered w-full" />
+                            <input type="text" name="photo_url" placeholder="Enter Photo url" className="input input-bordered w-full" />
                         </div>
                     </div>
                     <input className="btn btn-block bg-[#331A15] text-white mb-8" type="submit" value="Add Product" />
